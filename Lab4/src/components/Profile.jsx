10e33dc6f6f3519988aeb485501db9801cc438d5
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2023-04-06 01:47:42 +0700

    jh

---
+++ Lab4/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/1.jpeg" alt="Profile"/>
+            <h1 className="name">Mew Wannarath</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
